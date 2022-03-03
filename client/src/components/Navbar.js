025d54e5c0dffef7280377d67414beb17909ab40
David <rodpe.david@gmail.com>
2022-03-02 22:52:04 -0600

    Navbar and Sidebar html components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import {Link as LinkRouter} from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
+import {Link as LinkScroll} from 'react-scroll';
 
 
 
 const Navbar = ({toggle}) => {
   return (
-    <div>Navbar</div>
+    <nav className='nav-main'>
+      <nav className='navbar-container'>
+        <LinkRouter className='NavLogo' to ="/">
+          <h2>Logo</h2>
+        </LinkRouter>
+
+        <div className='mobile-icon' onClick={toggle}>
+          <FaBars/>
+        </div>
+        <ul className='nav-menu'>
+          <li className='nav-item'>
+            <LinkScroll to= 'about'>About</LinkScroll>
+          </li>
+        </ul>
+      </nav>
+    </nav>
   )
 }
 
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {FaTimes} from 'react-icons/fa';
+import {Link as LinkScroll} from 'react-scroll';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <aside isOpen = {isOpen} onClick={toggle}>
+      <div className='icon' onClick={toggle}>
+        <FaTimes/>
+      </div>
+      <div className='sidebar-wrapper'>
+        <ul className='sidebar-menu'>
+          <LinkScroll to ="about" onClick= {toggle}>
+            About
+          </LinkScroll>
+        </ul>
+      </div>
+    </aside>
   )
 }
 
