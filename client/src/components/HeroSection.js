8b3052183cfca9421a5ce3e7e7476d1e99785549
David <rodpe.david@gmail.com>
2022-03-03 15:56:28 -0600

    Hero section layout

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section className='hero'>
+      <img className='hero-img' src="https://i.postimg.cc/zD21m9fG/hero.png" alt="Pasta with pomodoro sauce"/>
+      <div className='info'>
+        <h1 className='info-title'>
+          Take your financial control to the next level
+        </h1>
+        <p className='info-subtitle'>
+          Create budgets and track your expenses with an easy integration.
+        </p>
+      </div>
+      
+    </section>
   )
 }
 
