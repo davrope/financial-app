f2bd6f85b4caf4380a296b9d14cbb151a982935b
David <rodpe.david@gmail.com>
2022-03-03 16:35:37 -0600

    About section layout

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,49 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='about'>This is the About component</div>
+    <section className='about'>
+      <h2 className='section-title'>
+        About
+      </h2>
+      <div className='cards-grid'>
+        <div id='about' className='card'>
+          <img src="https://i.postimg.cc/zD21m9fG/hero.png" alt="Pasta with pomodoro sauce" className='about-img'/>
+          <div className='text-card-container'>
+            <h4>
+              Create a budget
+            </h4>
+            <p>
+              We have the best tools so you can create a budget based on your income and goals.
+            </p>
+          </div>
+        </div>
+
+        <div id='about' className='card'>
+          <img src="https://i.postimg.cc/zD21m9fG/hero.png" alt="Pasta with pomodoro sauce" className='about-img'/>
+          <div className='text-card-container'>
+            <h4>
+              Create a budget
+            </h4>
+            <p>
+              We have the best tools so you can create a budget based on your income and goals.
+            </p>
+          </div>
+        </div>
+
+        <div id='about' className='card'>
+          <img src="https://i.postimg.cc/zD21m9fG/hero.png" alt="Pasta with pomodoro sauce" className='about-img'/>
+          <div className='text-card-container'>
+            <h4>
+              Create a budget
+            </h4>
+            <p>
+              We have the best tools so you can create a budget based on your income and goals.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
   )
 }
 
