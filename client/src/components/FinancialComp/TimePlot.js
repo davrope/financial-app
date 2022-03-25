18b05a479c054d946334ceebaf03f8aa82ddb60a
David <rodpe.david@gmail.com>
2022-03-24 20:41:24 -0600

    Data preprocess for plot

--- client/src/components/FinancialComp/TimePlot.js
+++ client/src/components/FinancialComp/TimePlot.js
@@ -1,23 +1,21 @@
 import React from 'react';
 import {VictoryChart, VictoryArea} from 'victory'
+import { useSelector } from 'react-redux';
 
 
 const TimePlot = () => {
-    const data = [
-        { x: new Date(2021, 5, 1), y: 8 },
-        { x: new Date(2021, 5, 2), y: 10 },
-        { x: new Date(2021, 5, 3), y: 7 },
-        { x: new Date(2021, 5, 4), y: 4 },
-        { x: new Date(2021, 5, 7), y: 6 },
-        { x: new Date(2021, 6, 8), y: 12 },
-        { x: new Date(2021, 5, 9), y: 7 },
-        { x: new Date(2021, 5, 10), y: 9 },
-        { x: new Date(2021, 5, 8), y: 13 }
-      ];
+  const transactions = useSelector((state)=>state.transactions)
 
-    //   const discontinuousScale = scaleDiscontinuous(
-    //     d3Scale.scaleTime()
-    //   ).discontinuityProvider(discontinuitySkipWeekends());
+  const data = transactions.map(function(obj){
+    let rObj = {};
+    rObj["x"] = new Date(obj.dateCreated).toLocaleDateString();
+    rObj["y"] = obj.amount;
+    return rObj
+  })
+ 
+
+  console.log(data)
+
 
   return (
     <VictoryChart 
