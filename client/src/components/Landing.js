28f21417653d943f928aab22979bb4c5664e2dab
David <rodpe.david@gmail.com>
2022-04-02 22:14:53 -0600

    Footer created

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import About from './About';
+import Footer from './Footer'
 
 const Landing = () => {
   return (
     <div>
         <HeroSection/>
         <About/>
+        <Footer/>
     </div>
   )
 }
